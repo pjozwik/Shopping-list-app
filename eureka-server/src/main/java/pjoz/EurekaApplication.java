3f82136c1f2f2473dda5aca7d6ba3321dbd47a1f
Piotrek Jóźwik <piotrek.jozwik12@gmail.com>
2022-11-14 18:46:40 +0100

    FTR-1 set up eureka server and clients

--- eureka-server/src/main/java/pjoz/EurekaApplication.java
+++ eureka-server/src/main/java/pjoz/EurekaApplication.java
@@ -2,13 +2,12 @@ package pjoz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class UserApplication {
+@EnableEurekaServer
+public class EurekaApplication {
     public static void main(String[] args) {
-
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 }
