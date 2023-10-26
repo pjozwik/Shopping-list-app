e091df60afe057a149f1ef33443ab1ef75c4fb5a
Piotrek <piotrek.jozwik12@gmail.com>
2023-10-26 19:52:08 +0200

    Setting up api gateway

--- user-service/src/main/java/pjoz/user/controller/UserController.java
+++ user-service/src/main/java/pjoz/user/controller/UserController.java
@@ -12,26 +12,26 @@ import pjoz.user.service.UserService;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 @AllArgsConstructor
 public class UserController {
 
     private final UserService userService;
     private final UserRepository userRepository;
 
-    @PostMapping(value = "/users/register")
+    @PostMapping(value = "/register")
     public ResponseEntity<String> crateNewUser(@RequestBody User user){
         //TODO check if user already exists
         userService.saveUser(user);
         return ResponseEntity.ok("User has been successfully added");
     }
 
-    @GetMapping(value = "/users/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<User> getUserById(@PathVariable int id){
         return ResponseEntity.of(userRepository.findById(id));
     }
 
-    @GetMapping(value = "/user")
+    @GetMapping(value = "/loggedUser")
     public Optional<UserDto> getLoggedInUserDetails(){
         var auth = SecurityContextHolder.getContext().getAuthentication();
         return userService.getUserByUserName("essa");
